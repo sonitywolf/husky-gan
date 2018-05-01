4e4efe5e48499d71e9c259a47f61793743426669
simonbdz <sabdz55@gmail.com>
2018-05-01 19:33:37 -0400

    Added some improvements

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # husky-gan
 Generative Adversarial Network written on TensorFlow to create cute huskies
+
+## Why Husky-GAN?
+Sonity Needs to learn from his husky-peers in order to become a original stand-alone husky 
--- dataset/download_dataset.py
+++ dataset/download_dataset.py
@@ -1,1 +1,15 @@
 # Script to download all the images contained in husky_images.json
+
+import urllib.request , json
+
+with open('husky_images.json') as json_data:
+    huskies = json.load(json_data)
+    huskies_total = len(huskies)
+    print(f"{huskies_total} loaded")
+
+for index, husky in enumerate(huskies):
+    print(f"Downloading {index}/{huskies_total}")
+    try:
+        urllib.request.urlretrieve(husky, f"img/{index}.jpg")
+    except:
+        print("\033[93mError!")
